6626359af6ae9799aa2f4234ac162c1f7fb42614
fritz-astronomer <fritz@astronomer.io>
2022-12-21 16:53:33 -0500

    remove airflow as a install_requires

--- airflow/providers/package.py
+++ airflow/providers/package.py
@@ -8,7 +8,7 @@ def get_provider_info() -> Dict[str, Any]:
         "package-name": "apache-airflow-provider-alembic",
         "name": "apache-airflow-provider-alembic",
         "description": "An Apache Airflow Providers for Alembic Operators and Hooks from Astronomer",
-        "versions": "0.1.1",
+        "versions": "0.1.2",
         # Optional.
         "hook-class-names": [],
         "extra-links": [],
